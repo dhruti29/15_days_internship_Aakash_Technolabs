b9fc2858b1783bc46e0f662beba1b13add570eeb
Dhruti Gandhi <66105257+dhruti29@users.noreply.github.com>
2021-06-14 18:25:39 +0530

    Add files via upload

---
+++ task-4/function.py
@@ -0,0 +1,15 @@
+def myfun(n1,n2):
+        return n1+n2
+
+
+n1 = int(input("enter 1st number:"))
+n2 = int(input("enter 2nd number:"))
+print(myfun(n1,n2))
+
+
+def fun(name):
+        print("hello",name)
+
+
+name = input("enter your name:")
+fun(name)
